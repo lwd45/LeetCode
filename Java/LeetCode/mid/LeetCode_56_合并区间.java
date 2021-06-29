2ecdfe1242d849b10a9820c21eb173fd370497de
luoweidong <912915121@qq.com>
2021-06-30 01:34:35 +0800

    add git ignore

--- Java/LeetCode/mid/LeetCode_56_合并区间.java
+++ Java/LeetCode/mid/LeetCode_56_合并区间.java
@@ -30,7 +30,7 @@ public class LeetCode_56_合并区间 {
         int[][] ans = new int[list.size()][2];
         for (int i = 0; i < list.size(); ++i) {
             ans[i][0] = list.get(i)[0];
-            ans[i][0] = list.get(i)[1];
+            ans[i][1] = list.get(i)[1];
         }
         return ans;
     }
