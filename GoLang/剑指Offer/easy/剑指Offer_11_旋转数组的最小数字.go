1ad181b8db48c77f43f0b97f30827844ab8753c7
lwd <912915121@qq.com>
2021-07-05 23:58:48 +0800

    Go 剑指Offer_11_旋转数组的最小数字.go

--- GoLang/剑指Offer/easy/剑指Offer_11_旋转数组的最小数字.go
+++ GoLang/剑指Offer/easy/剑指Offer_11_旋转数组的最小数字.go
@@ -1,1 +1,16 @@
 package easy
+
+func minArray(numbers []int) int {
+	left, right := 0, len(numbers)-1
+	for left < right {
+		mid := (left + right) >> 1
+		if numbers[mid] > numbers[right] {
+			left = mid + 1
+		} else if numbers[mid] < numbers[right] {
+			right = mid
+		} else {
+			right-- //因为相等，则左侧至少有一个替代品
+		}
+	}
+	return numbers[left]
+}
