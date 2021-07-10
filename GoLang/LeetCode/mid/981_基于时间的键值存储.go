dbf10ff1db4937c62d623daa53b919d27206083b
lwd <912915121@qq.com>
2021-07-10 15:32:24 +0800

    Go 981_基于时间的键值存储.go

---
+++ GoLang/LeetCode/mid/981_基于时间的键值存储.go
@@ -0,0 +1,32 @@
+package mid
+
+import "sort"
+
+type pair struct {
+	timestamp int
+	value     string
+}
+
+type TimeMap struct {
+	m map[string][]pair
+}
+
+func Constructor() TimeMap {
+	return TimeMap{m: map[string][]pair{}}
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	this.m[key] = append(this.m[key], pair{
+		timestamp: timestamp,
+		value:     value,
+	})
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	pairs := this.m[key]
+	i := sort.Search(len(pairs), func(i int) bool { return pairs[i].timestamp > timestamp })
+	if i > 0 {
+		return pairs[i].value
+	}
+	return ""
+}
