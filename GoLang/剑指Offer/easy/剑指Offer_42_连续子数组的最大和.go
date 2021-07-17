1a256fdcd17867d96960a1aed7ba850e05e9f320
luoweidong <912915121@qq.com>
2021-07-17 11:12:27 +0800

    剑指Offer_42_连续子数组的最大和.go

--- GoLang/剑指Offer/easy/剑指Offer_42_连续子数组的最大和.go
+++ GoLang/剑指Offer/easy/剑指Offer_42_连续子数组的最大和.go
@@ -1,1 +1,16 @@
 package easy
+
+func maxSubArray(nums []int) int {
+	max, sum := nums[0], 0
+
+	for _, num := range nums {
+		sum += num
+		if sum > max {
+			max = sum
+		}
+		if sum < 0 {
+			sum = 0
+		}
+	}
+	return max
+}
