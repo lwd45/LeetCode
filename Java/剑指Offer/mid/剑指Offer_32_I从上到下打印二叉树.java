51ba89408c87db71d07712b37540a324beca5992
luoweidong <912915121@qq.com>
2021-07-26 00:17:09 +0800

    剑指Offer_32_I从上到下打印二叉树

---
+++ Java/剑指Offer/mid/剑指Offer_32_I从上到下打印二叉树.java
@@ -0,0 +1,48 @@
+package mid;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by lwd at 2021/7/26
+ *
+ * @Description:
+ */
+public class 剑指Offer_32_I从上到下打印二叉树 {
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) return null;
+
+        ArrayList<TreeNode> ans = new ArrayList<>();
+        LinkedList<TreeNode> list = new LinkedList<>();
+        list.addLast(root);
+        ans.add(root);
+        while (!list.isEmpty()) {
+            TreeNode first = list.pollFirst();
+            if (first.left != null) {
+                list.addLast(first.left);
+                ans.add(first.left);
+            }
+            if (first.right != null) {
+                list.addLast(first.right);
+                ans.add(first.right);
+            }
+        }
+        int[] ret = new int[ans.size()];
+        for (int i = 0; i < ret.length; ++i) {
+            ret[i] = ans.get(i).val;
+        }
+        return ret;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
