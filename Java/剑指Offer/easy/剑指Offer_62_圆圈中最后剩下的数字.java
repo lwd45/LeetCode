d5be5f53438360a20a88c97848a49dcb274626ed
luoweidong <912915121@qq.com>
2021-08-10 00:01:53 +0800

    剑指Offer/easy/剑指Offer_62_圆圈中最后剩下的数字.java

--- Java/剑指Offer/easy/剑指Offer_62_圆圈中最后剩下的数字.java
+++ Java/剑指Offer/easy/剑指Offer_62_圆圈中最后剩下的数字.java
@@ -1,6 +1,16 @@
-package easy;/**
+package easy;
+
+/**
  * Created by lwd at 2021/8/8
  *
  * @Description:
- */public class 剑指Offer_62_圆圈中最后剩下的数字 {
+ */
+public class 剑指Offer_62_圆圈中最后剩下的数字 {
+    public int lastRemaining(int n, int m) {
+        int ans = 0;
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + m) % i;
+        }
+        return ans;
+    }
 }
