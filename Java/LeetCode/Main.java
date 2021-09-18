6b1931f2f97eefce15eb85f9d2ff647c436a6f39
luoweidong <912915121@qq.com>
2021-09-18 11:19:30 +0800

    -

---
+++ Java/LeetCode/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long b = scanner.nextLong();
+        long a = scanner.nextLong();
+        System.out.println(a + b);
+    }
+}
