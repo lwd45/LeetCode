b91b2a9be426b0f7cdab7893ddb78a697f6d35d9
luoweidong <912915121@qq.com>
2021-11-04 15:30:17 +0800

    367_有效的完全平方数.go

---
+++ GoLang/LeetCode/easy/367_有效的完全平方数.go
@@ -0,0 +1,16 @@
+package easy
+
+func isPerfectSquare(num int) bool {
+	left, right := 1, num
+	for ; left <= right; {
+		mid := (left + right) / 2
+		if mid*mid == num {
+			return true
+		} else if mid*mid > num {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return false
+}
