49bd24a4dab93a73ebb95df35858e1a03c9fefb3
luoweidong <912915121@qq.com>
2021-11-19 15:59:36 +0800

    LeetCode/mid/397_整数替换.go

---
+++ GoLang/LeetCode/mid/397_整数替换.go
@@ -0,0 +1,11 @@
+package mid
+
+func integerReplacement(n int) int {
+	if n == 1 {
+		return 0
+	}
+	if n%2 == 0 {
+		return 1 + integerReplacement(n/2)
+	}
+	return 2 + min(integerReplacement(n/2), integerReplacement((n+1)/2))
+}
--- GoLang/main.go
+++ GoLang/main.go
@@ -4,4 +4,5 @@ func main() {
 	//easy.FindWords([]string{"Hello", "Alaska", "Dad", "Peace"})
 	//println(mid.GetMoneyAmount(10))
 	//mid.MaxProduct([]string{"aa"})
+	//mid.IntegerReplacement(100000000)
 }
