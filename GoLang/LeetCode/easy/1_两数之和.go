f2a48edfc6dedd72a3dc9d3eae4b9aab14fa1c7a
luoweidong <912915121@qq.com>
2023-07-01 19:09:37 +0800

    week day daily

--- GoLang/LeetCode/easy/1_两数之和.go
+++ GoLang/LeetCode/easy/1_两数之和.go
@@ -1,25 +1,36 @@
 package easy
 
-func twoSum(nums []int, target int) []int {
-	mp := make(map[int][]int)
-	for i, num := range nums {
-		if _, ok := mp[num]; ok {
-			mp[num] = append(mp[num], i)
-		} else {
-			mp[num] = []int{i}
-		}
-	}
+//func twoSum(nums []int, target int) []int {
+//	mp := make(map[int][]int)
+//	for i, num := range nums {
+//		if _, ok := mp[num]; ok {
+//			mp[num] = append(mp[num], i)
+//		} else {
+//			mp[num] = []int{i}
+//		}
+//	}
+//
+//	for i, num := range nums {
+//		if value, ok := mp[target-num]; ok {
+//			if target-num == num {
+//				if len(value) > 1 {
+//					return []int{value[0], value[1]}
+//				}
+//			} else {
+//				return []int{i, value[0]}
+//			}
+//		}
+//	}
+//	return []int{-1, -1}
+//}
 
-	for i, num := range nums {
-		if value, ok := mp[target-num]; ok {
-			if target-num == num {
-				if len(value) > 1 {
-					return []int{value[0], value[1]}
-				}
-			} else {
-				return []int{i, value[0]}
-			}
+func twoSum(nums []int, target int) []int {
+	idxMap := make(map[int]int)
+	for idx, num := range nums {
+		if _, exist := idxMap[target-num]; exist {
+			return []int{idxMap[target-num], idx}
 		}
+		idxMap[num] = idx
 	}
-	return []int{-1, -1}
+	return nil
 }
